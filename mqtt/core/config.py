0358032b3d0c61176943e4909d626f21e53f0da5
Kyle (Zihao Chen) <zihaokylechen@gmail.com>
2021-10-23 19:51:05 -0700

    Update config.py

--- mqtt/core/config.py
+++ mqtt/core/config.py
@@ -1,5 +1,7 @@
 import os
 
+MQTT_URL = os.environ.get('MQTT_URL', 'mqtt://localhost:1883')
+
 # MQTT broker configuration
 MQTT_BROKER = os.environ.get("MQTT_BROKER","localhost")
 MQTT_BROKER_PORT = int(os.environ.get("MQTT_BROKER_PORT",1883))
