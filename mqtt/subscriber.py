225c97f31206928de55e2e08789be00a168e764c
Kyle Chen <zihaokylechen@gmail.com>
2021-10-24 00:12:36 -0700

    updates

--- mqtt/subscriber.py
+++ mqtt/subscriber.py
@@ -39,42 +39,44 @@ def process_message(client,msg):
     print(msg.topic + " " + str(msg.payload))
     #print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-    message = json.loads(msg.payload.decode())
+    #message = json.loads(msg.payload.decode())
+    message = str(msg.payload)
 
     topic = msg.topic
     if topic == MQTT_CAMERA_TOPIC:
         img = message.get("photo",None)
-        msg = message.get("message",None)
+        #msg_camera = message.get("message",'')
+
         trusted=False
         #TO DO 
         # what to do with image
         if img:
-            print("image: " + img)
-            trusted=True
-
+            trusted = True
+            # trusted = False
+            pass
+            
         #TO DO
         #What to do after receiving message from camera esp32
-        if msg:
-            client.publish(MQTT_SERVO_TOPIC, msg) #redirecting message from camera to servo
+        #if msg_camera:
+        #    print(msg_camera)
+        if trusted:
+            client.publish(MQTT_SERVO_TOPIC, "Trusted person")
         else:
-            msg = ''
-            if trusted:
-                msg = "Trusted person"
-            else:
-                msg = "Not a trusted person"
+            client.publish(MQTT_SERVO_TOPIC, "Not a trusted person")
             
-            client.publish(MQTT_SERVO_TOPIC, "Empty message from ESP32 camera" + ' ' + str(msg))
+        print("message from esp32/camera: " + message)
 
     elif topic == MQTT_SERVO_TOPIC:
-        msg = message.get("servo_status",None)
+        msg_servo = message.get("servo_status",None)
 
         #TO DO 
         #What to do after receiving message from servo esp32
-        if msg:
-            client.publish(MQTT_CAMERA_TOPIC, msg) #redirecting message from servo to camera
+        if msg_servo:
+            # client.publish(MQTT_CAMERA_TOPIC, msg_servo) #redirecting message from servo to camera
+            print("servo_status: " + msg_servo)
         else:
-            client.publish(MQTT_CAMERA_TOPIC, "Empty message from ESP32 servo")
-        
+            # client.publish(MQTT_CAMERA_TOPIC, "Empty message from ESP32 servo")
+            print("message from servo: " + message)
         
 def main():
     client = connect_mqtt_broker(cb_connect=subscribe)
