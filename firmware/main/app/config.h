1a081e1fb3265ec0d6cc2f74c7b979c4041b60f0
lhy2016 <lhy920104@gmail.com>
2021-11-28 23:39:37 -0800

    Update config.h

--- firmware/main/app/config.h
+++ firmware/main/app/config.h
@@ -2,6 +2,6 @@
 #define CONFIG_H
 
 #define MQTT_PUBLISH_TOPIC      "devices/info/lock"
-#define MQTT_SUBSCRIBE_TOPIC    "hub/control/lock"
+#define MQTT_SUBSCRIBE_TOPIC    "hub/control/lock/MYFIRSTLOCK001"
 
 #endif
